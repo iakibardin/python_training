b7be6469a45777621a2800fd9c0d1a192e6d07c8
iakibardin <kibardin90@gmail.com>
2017-08-09 13:56:53 +0300

    Добавлен тест на редактирование группы

---
+++ test/test_edit_group.py
@@ -0,0 +1,6 @@
+from model.group import Group
+
+def test_edit_first_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_first_group(Group(name="22222", header="2222", footer="222"))
+    app.session.logout()
