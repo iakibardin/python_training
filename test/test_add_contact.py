c2dc11525d68b9dd7d2bf0736507c6ce993b40ea
iakibardin <kibardin90@gmail.com>
2017-08-09 10:33:42 +0300

    Добавлена общая фикстура conftest.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,21 +1,9 @@
 # -*- coding: utf-8 -*-
-import unittest
-
-import pytest
-
-from fixture.application import Application
 from model.contact import Contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
         app.session.login(username="admin", password="secret")
-        app.contact.create(Contact(firstname="test", middlename="test", lastname="test", nickname="test", photo_directory="C:\\Users\\ikibardin\\Desktop\\screen.jpg", title="test",
+        app.contact.create(Contact(firstname="test", middlename="test", lastname="test", nickname="test", photo_directory="C:\\Users\\ikibardin\\Desktop\\newscreen.jpg",                              title="test",
                                    company="test", address="test", home_number="11111", mobile_number="22222", work_number="33333", fax="44444", email1="mail1", email2="mail2", email3="mail3", homepage="test.com",
                                    day_of_birth="//div[@id='content']/form/select[1]//option[3]",
                                    month_of_birth="//div[@id='content']/form/select[2]//option[2]", year_of_birth="1990",
@@ -23,6 +11,3 @@ def test_add_contact(app):
                                    month_of_annivesary="//div[@id='content']/form/select[4]//option[2]", year_of_annivesary="2010", address2="test test test 1",
                                    home_number2="test test test 3", note="testnote"))
         app.session.logout()
-
-if __name__ == '__main__':
-    unittest.main()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,17 +1,5 @@
 # -*- coding: utf-8 -*-
-import unittest
-
-import pytest
-
-from fixture.application import Application
 from model.group import Group
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
@@ -23,6 +11,3 @@ def test_add_empty_group(app):
     app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
-
-if __name__ == '__main__':
-    unittest.main()
