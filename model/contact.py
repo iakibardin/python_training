03a9bcfb7207918f4b9b59eb9e463b9f5dbab0c8
iakibardin <kibardin90@gmail.com>
2017-09-06 18:34:45 +0300

    Добавлена случайная генерация строк при создании групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -35,7 +35,7 @@ class Contact:
         self.all_emailes_from_home_page = all_emailes_from_home_page
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.firstname)
+        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname \
