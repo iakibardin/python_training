51bdc5ce6ea12d03f6c8d75140a58484410e46bf
iakibardin <kibardin90@gmail.com>
2017-09-13 10:38:08 +0300

    Загрузка информации из конфига

--- conftest.py
+++ conftest.py
@@ -2,22 +2,26 @@ import pytest
 from fixture.application import Application
 from selenium.webdriver.common.keys import Keys
 
-
+import os.path
+import json
 fixture = None
-
+target = None
 
 @pytest.fixture
 def app(request):
     global fixture
-    if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseUrl")
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as config_file:
+            target = json.load(config_file)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target['baseUrl'])
+    fixture.session.ensure_login(username=target['username'], password=target['password'])
     return fixture
+
+
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
@@ -29,4 +33,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
-    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--target", action="store", default="target.json")
